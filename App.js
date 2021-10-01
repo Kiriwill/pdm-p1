f10dc184d133887181f0934ca908480a43cec1ed
Willian Pacheco <willian.pacheco@solidaridad.org>
2021-10-01 18:53:39 -0300

    inserindo bootstrap

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Button, Text, TextInput,  View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>TESTE</Text>
     </View>
   );
 }
